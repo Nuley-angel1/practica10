3ba4c75c5e0e84179c86c47942f1b6994037dbd3
Nuley-angel1 <nuley@live.com.mx>
2017-03-13 17:10:26 -0600

    Add files via upload

---
+++ practica10/index.php
@@ -0,0 +1,15 @@
+<?php
+//comentarios
+#comentarios
+/*comentarios
+multilinea*/
+echo "hola mundo php";
+?>
+<html>
+<head>
+<title>practica10 php</title>
+<body>
+<h1> hola mundo html </h1>
+</body>
+</head>
+</html>
---
+++ practica10/index2.php
@@ -0,0 +1,17 @@
+
+<html>
+<head>
+<title>practica10 php</title>
+</head>
+<body>
+         <h1>usando PHP </h1>
+         <p><?php
+		 echo "parrafo php";
+		 ?></p>
+		 <p>parrafo HTML</p>
+		 <h3>
+		 <p><?php
+		 echo "parrafo php";
+		 ?>
+</body>
+</html>
